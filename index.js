767b360f2d43cefa8c023d405de623c650ce4c64
Anak Mirasing <thaihooligan@hotmail.com>
2017-01-18 17:54:45 +0700

    Update Facebook token verification method

--- index.js
+++ index.js
@@ -9,24 +9,27 @@ var config = require('./config');
 app.set('port', (process.env.PORT || 5000));
 
 // Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({extended: false}));
 
 // Process application/json
-app.use(bodyParser.json())
+app.use(bodyParser.json());
 
 // Index route
 app.get('/', function (req, res) {
     res.send('Hey! I am MR.Nbe :D')
-})
+});
 
 // Facebook verification
 app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === config.VERIFY_TOKEN) {
-        res.send(req.query['hub.challenge'])
+    if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.VERIFY_TOKEN) {
+    	console.log("Token validation success.")
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+    	console.log("Token validation failed.")
+    	res.sendStatus(403);
     }
-    res.send('Error, wrong token')
-})
+});
 
 app.listen(app.get('port'), function() {
     console.log('Running NBE bot on port', app.get('port'))
-})
+});
